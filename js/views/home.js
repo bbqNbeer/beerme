1e1e63b3f7905ddf5cce306694498208b1e381c4
root <root@bizquik.(none)>
2013-07-24 23:59:40 +0000

    added home view

--- js/views/home.js
+++ js/views/home.js
@@ -1,1 +1,28 @@
-// home
+var HomeView = Backbone.View.extend({
+    el: 'body',
+    template: _.template( $( '#homeTemplate' ).html() ),
+
+    initialize:function () {
+        this.render();
+    },
+ 
+    render:function () {
+        $(this.el).html(this.template());
+        return this;
+    },
+
+    events:{
+        "click #searchButton":"search",
+        "click #locateBreweries":"geoLocate",
+    },
+
+    search: function(event){
+       console.log('search');
+       //var searchItems = new BeerCollection(beerTest);
+       new BeerListView( beerTest );
+    },
+
+    geoLocate: function(event){
+
+    }
+});
